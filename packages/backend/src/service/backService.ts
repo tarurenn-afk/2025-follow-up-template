8f471bb551fb14e64ff7f407f4787172e706a235
tarurenn-afk <tarurenn@gmail.com>
2026-03-20 00:36:38 +0900

    Add files via upload

--- packages/backend/src/service/backService.ts
+++ packages/backend/src/service/backService.ts
@@ -1,18 +1,85 @@
 import { pool } from '@/db'
-import type { RowDataPacket } from 'mysql2'
-import type { User } from '@/types'
+import type { RowDataPacket, ResultSetHeader } from 'mysql2'
+import type { addUser, editUser } from '@/types'
 
-export const getAllUsers = async (): Promise<User[]> => {
+export const getAllTodos = async () => {
   try {
-    const [rows] = await pool.query<RowDataPacket[]>('SELECT * FROM users')
+    const [rows] = await pool.query<RowDataPacket[]>(
+      'SELECT id, title, content, created_at, updated_at FROM users ORDER BY id ASC'
+    )
     return rows.map((row) => ({
       id: row.id,
-      name: row.name,
-      eMail: row.e_mail,
-      createdAt: row.created_at.toISOString()
+      title: row.title,
+      content: row.content,
+      createdAt: row.created_at,
+      updatedAt: row.updated_at
     }))
   } catch (error) {
-    console.error('Error fetching users:', error)
+    console.error('Error fetching todo:', error)
+    throw error
+  }
+}
+
+export const getTodoNo = async (id: number) => {
+  try {
+    const [rows] = await pool.query<RowDataPacket[]>(
+      'SELECT id, title, content, created_at, updated_at FROM users WHERE id = ?',
+      [id]
+    )
+
+    if (rows.length === 0) return null
+
+    const row = rows[0]
+    return {
+      id: row.id,
+      title: row.title,
+      content: row.content,
+      createdAt: row.created_at,
+      updatedAt: row.updated_at
+    }
+  } catch (error) {
+    console.error('Error fetching todo by id:', error)
+    throw error
+  }
+}
+
+export const addTodo = async (data: addUser) => {
+  const { title, content } = data
+  try {
+    const [result] = await pool.query<ResultSetHeader>(
+      'INSERT INTO users (title, content, created_at, updated_at) VALUES (?, ?, NOW(), NOW())',
+      [title, content]
+    )
+    return result
+  } catch (error) {
+    console.error('Error inserting todo:', error)
+    throw error
+  }
+}
+
+export const updateTodo = async (data: editUser) => {
+  const { title, content, id } = data
+  try {
+    const [result] = await pool.query<ResultSetHeader>(
+      'UPDATE users SET title = ?, content = ?, updated_at = NOW() WHERE id = ?',
+      [title, content, id]
+    )
+    return result
+  } catch (error) {
+    console.error('Error updating todo:', error)
+    throw error
+  }
+}
+
+export const deleteTodo = async (id: number) => {
+  try {
+    const [result] = await pool.query<ResultSetHeader>(
+      'DELETE FROM users WHERE id = ?',
+      [id]
+    )
+    return result
+  } catch (error) {
+    console.error('Error deleting todo:', error)
     throw error
   }
 }
