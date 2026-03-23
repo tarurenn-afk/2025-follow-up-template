89b821333540dcaa41a3d7985b380ab88f118634
tarurenn-afk <tarurenn@gmail.com>
2026-03-24 03:20:40 +0900

    Add files via upload

--- .devcontainer/script/init.sql
+++ .devcontainer/script/init.sql
@@ -1,9 +1,8 @@
 -- サンプルテーブル
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  e_mail VARCHAR(100) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  title VARCHAR(20) NOT NULL,
+  content VARCHAR(200) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
--- サンプルデータ
-INSERT INTO users (name, e_mail) VALUES ('Taro Yamada', 'taro.yamada@localhost.com'), ('Saki Abe', 'saki.abe@localhost.com');
