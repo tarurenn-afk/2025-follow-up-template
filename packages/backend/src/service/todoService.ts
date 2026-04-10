d23ee6a75dd4ae31054647c7cb7dbccaeb5942a9
Taichiro Takashima <t.takashima@no4.co.jp>
2026-04-10 18:05:45 +0900

    定数名変更

--- packages/backend/src/service/todoService.ts
+++ packages/backend/src/service/todoService.ts
@@ -21,7 +21,7 @@ export const getAllTodos = async () => {
   }
 }
 
-export const getTodoId = async (id: number) => {
+export const getTodoById = async (id: number) => {
   try {
     const [rows] = await pool.query<RowDataPacket[]>(
       'SELECT id, title, content, time_limit,created_at, updated_at FROM todos WHERE id = ?',
