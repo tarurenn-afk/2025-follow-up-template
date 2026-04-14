2a33f50aca5c9dd94ad7706a89c50450903d6a8f
Taichiro Takashima <t.takashima@no4.co.jp>
2026-04-14 14:42:51 +0900

    hooks追加

---
+++ packages/frontend/src/app/hooks/useTodo.ts
@@ -0,0 +1,27 @@
+import useSWR, { type KeyedMutator } from 'swr'
+import type { Todo } from '@/shared/types'
+
+export function useTodo(id: number): {
+  todos: Todo
+  error: Error | undefined
+  isLoading: boolean
+  mutate: KeyedMutator<Todo>
+} {
+  const fetcher = async (url: string): Promise<Todo> => {
+    const res = await fetch(url)
+    if (!res.ok) {
+      const status = res.status
+      const body = await res.text()
+      const message = `failed to fetch todos. error(status: ${status}, body: ${body})`
+      throw new Error(message)
+    }
+    return res.json()
+  }
+
+  const { data, error, isLoading, mutate } = useSWR<Todo, Error>(
+    `${process.env.NEXT_PUBLIC_API_URL}/todos/${id}`,
+    fetcher
+  )
+
+  return { todos: data, error, isLoading, mutate }
+}
